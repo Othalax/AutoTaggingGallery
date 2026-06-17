f6d6155e92f46a188dd11b6b55f3e59b3ceb1229
Othalax <dominisiajarzabek@gmail.com>
2026-06-17 20:50:46 +0200

    Added configuration file

--- detection.py
+++ detection.py
@@ -1,11 +1,15 @@
 from ultralytics import YOLO
 import os
+import configparser
 
 class Detector:
     def __init__(self):
         appdata_dir = os.getenv('APPDATA')
         self.storage_dir = os.path.join(appdata_dir, "AutoTaggingGallery", 'yolov8n-oiv7.pt')
         self.model = YOLO(self.storage_dir)
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.confidence_threshold = float(config['Detector']['confidence_threshold'])
 
     def detect_tags(self, filepath):
         results = self.model(filepath)
@@ -16,7 +20,7 @@ class Detector:
                 for box in result.boxes:
                     conf = float(box.conf[0])
 
-                    if conf > 0.3:
+                    if conf > self.confidence_threshold:
                         class_id = int(box.cls[0])
                         tag_name = result.names[class_id]
 
